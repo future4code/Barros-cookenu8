258f9c8fab99821b696eeb19f39c95bd5e16d228
Maria Fernandez de Moura Ferro <mariafmferro@gmail.com>
2023-02-09 20:12:53 -0300

    Add create-user funcionalities - not finished

--- src/business/UserBusiness.ts
+++ src/business/UserBusiness.ts
@@ -1,14 +1,35 @@
 import { UserDatabase } from "../data/UserDatabase";
+import { CustomError } from "../error/CustomError";
 import { UserInputDTO } from "../model/userDTO";
+import { Authenticator } from "../services/Authenticator";
+import { HashManager } from "../services/HashManager";
+import { IdGenerator } from "../services/IdGenerator";
 
 const userDatabase = new UserDatabase()
+const idGenerator = new IdGenerator()
+const authenticator = new Authenticator()
+const hashManager = new HashManager()
 
 export class UserBusiness {
-    createUser = async (input: UserInputDTO): Promise<void> => {
+    createUser = async (input: UserInputDTO) => {
         try {
-            
-        } catch (error) {
-            
+            const { name, email, password } = input
+
+            //adicionar todas as verificações
+
+            const id: string = idGenerator.generateId()
+            const cypherPassword = await hashManager.hash(password)
+
+            const newUser = {
+                id, name, email, password: cypherPassword
+            }
+
+            await userDatabase.createUser(newUser)
+            const token = authenticator.generateToken({id})
+            return token
+
+        } catch (error:any) {
+            throw new CustomError(error.statusCode, error.message);
         }
     }
 }
---
+++ src/error/UserErrors.ts
@@ -0,0 +1,1 @@
+// adicionar erros customizados
